90f8b14aa3ed2a4b3adbac98136b4dbc037d68e8
yuwantha98 <yuwantharavihara123@gmail.com>
2024-07-02 12:57:26 +0530

    input number get

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-// console.log("hello");
-
-// class Customer {
-//   name;
-//   age;
-//   address;
-
-//   constructor(name, age, address) {
-//     this.name = name;
-//     this.age = age;
-//     this.address = address;
-//   }
-//   setName(name) {
-//     this.name = name;
-//   }
-//   getName() {
-//     return this.name;
-//   }
-// }
-
-// let customer1 = new Customer("yuwa", 12, "balapitiya");
-
-// console.log(customer1.getName());
-
 let randomNumber = Math.floor(Math.random() * 10 + 1);
 
 console.log(randomNumber);
+
+function getNumber() {
+  var number = document.getElementById("numberInput").value;
+  console.log("The number entered is: " + number);
+}
