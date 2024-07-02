c7512f25cd65bd72ce10dc3fa2ca71bac003ea85
yuwantha98 <yuwantharavihara123@gmail.com>
2024-07-02 12:33:10 +0530

    create game interface

---
+++ app.js
@@ -0,0 +1,27 @@
+// console.log("hello");
+
+// class Customer {
+//   name;
+//   age;
+//   address;
+
+//   constructor(name, age, address) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+//   setName(name) {
+//     this.name = name;
+//   }
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// let customer1 = new Customer("yuwa", 12, "balapitiya");
+
+// console.log(customer1.getName());
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+console.log(randomNumber);
