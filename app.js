6e8ad9051cd10c25a52a6eacf0a306642c7e2e5e
yuwantha98 <yuwantharavihara123@gmail.com>
2024-07-02 14:29:11 +0530

    Apply changes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+let randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+console.log(randomNumber);
+
 function changRange() {
   let min = parseInt(document.getElementById("numberRangeStart").value);
   let max = parseInt(document.getElementById("numberRangeEnd").value);
 
-  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
   console.log(randomNumber);
 }
@@ -11,12 +14,12 @@ function getNumber() {
   var number = parseInt(document.getElementById("numberInput").value, 10);
   if (number === randomNumber) {
     document.getElementById("print").innerHTML =
-      "You guessed the number correctly!";
+      "You guessed the number correctly 😊";
   } else if (number > randomNumber) {
     document.getElementById("print").innerHTML =
-      "Your guess is greater than the number.";
+      "Your guess is greater than the number 😟";
   } else if (number < randomNumber) {
     document.getElementById("print").innerHTML =
-      "Your guess is less than the number.";
+      "Your guess is less than the number 😟";
   }
 }
