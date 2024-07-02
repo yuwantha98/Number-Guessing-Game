a11d8a35efe9280cfbda3e99b48641fc4896e1cf
yuwantha98 <yuwantharavihara123@gmail.com>
2024-07-02 13:17:51 +0530

    Add css file

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let randomNumber = Math.floor(Math.random() * 10 + 1);
-
 console.log(randomNumber);
 
 function getNumber() {
-  var number = document.getElementById("numberInput").value;
-  console.log("The number entered is: " + number);
+  var number = parseInt(document.getElementById("numberInput").value, 10);
+  if (number === randomNumber) {
+    document.getElementById("print").innerHTML =
+      "You guessed the number correctly!";
+  } else if (number > randomNumber) {
+    document.getElementById("print").innerHTML =
+      "Your guess is greater than the number.";
+  } else if (number < randomNumber) {
+    document.getElementById("print").innerHTML =
+      "Your guess is less than the number.";
+  }
 }
